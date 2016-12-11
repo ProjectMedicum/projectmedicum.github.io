ed334ae56ebe2cd0914cea17f8a4e15df5b5f900
Kacper <johnbaton12312@hotmail.com>
2016-12-11 02:59:58 +0100

    Aktualizacja #2 skryptu losującego choroby

--- JavaScript/RandomPage.js
+++ JavaScript/RandomPage.js
@@ -1,20 +1,13 @@
 	var links = new Array()
 	links[ 0 ] = "Figowka_Gronkowcowa.html"
-	links[ 1 ] = "Dis2.html"
-	links[ 2 ] = "Dis3.html"
-	links[ 3 ] = "Dis4.html"
-	links[ 4 ] = "Dis5.html"
-	links[ 5 ] = "Dis6.html"
-	links[ 6 ] = "Dis7.html"
-	links[ 7 ] = "Dis8.html"
-	links[ 8 ] = "Dis9.html"
-	links[ 9 ] = "Dis10.html"
-	links[ 10 ] = "Dis11.html"
+	links[ 1 ] = "Borelioza.html"
 
 	function RandomizePage() {
 		var a = Math.floor( Math.random() * links.length );
 		var i = a;
 		var last = links[ links.length - 1 ];
+		parent.location = [ i ];		
+		/*
 		links.push( "Error.html" );
 
 		if ( i > last ) {
@@ -23,4 +16,5 @@
 			parent.location = links[ i ];
 		}
 		return false;
+		*/
 	}
