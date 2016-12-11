689c47110c2563a0b5c79b93db3ec286597c62bf
Fayacre <johnbaton12312@hotmail.com>
2016-12-11 05:44:03 +0100

    Uporządkowanie kodu

--- JavaScript/RandomPage.js
+++ JavaScript/RandomPage.js
@@ -1,20 +1,35 @@
-	var links = new Array()
-	links[ 0 ] = "Figowka_Gronkowcowa.html"
-	links[ 1 ] = "Borelioza.html"
 
-	function RandomizePage() {
-		var a = Math.floor( Math.random() * links.length );
-		var i = a;
-		var last = links[ links.length - 1 ];
-		parent.location = [ i ];		
-		/*
-		links.push( "Error.html" );
+//NIE CZYŚCIĆ KODU!
 
-		if ( i > last ) {
-			parent.location = links[ last ];
-		} else {
-			parent.location = links[ i ];
-		}
-		return false;
-		*/
+var links = new Array()
+links[ 0 ] = "Figowka_Gronkowcowa.html"
+links[ 1 ] = "Borelioza.html"
+
+/*------------- Nie dotykać kodu pod tą linią -------------*/
+
+function RandomizePage() {
+	var a = Math.floor( Math.random() * links.length );
+	var i = a;
+	var last = links[ links.length - 1 ];
+	parent.location = links [ i ];
+	
+	/*
+	links.push( "Error.html" );
+	if ( i > last ) {
+		parent.location = links[ last ];
+	} else {
+		parent.location = links[ i ];
 	}
+	return false;
+	parent.location = links[ i ];
+	*/
+
+}
+
+/* 
+	Jak dodać kolejne choroby:
+		1. Skopiuj dokładną nazwę pliku choroby, np. "Osteoporoza.html".
+		2. Dodaj linię kodu "links[ x ] = "Plik.html", gdzie 'x' jest liczbą następującą po poprzedniej chorobie.
+		3. ???
+		4. Profit.
+*/
