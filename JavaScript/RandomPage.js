f35d5c9663164ddeeeba507589d1b0e0a5219e0e
Fayacre <johnbaton12312@hotmail.com>
2016-12-15 20:51:30 +0100

    Revert "Usunięcie zbędnego kodu | Prettify"
    This reverts commit d91d5bfb5f1a582d11cb863d2f7c8ddfe61873a3.

--- JavaScript/RandomPage.js
+++ JavaScript/RandomPage.js
@@ -1,16 +1,38 @@
+
 //NIE CZYŚCIĆ KODU!
 
-var links = new Array();
-links[0] = "../Disease/Borelioza.html";
-links[0] = "../Disease/Cukrzycowa_Choroba_Nerek.html";
-links[2] = "../Disease/Dna_Moczanowa.html";
-links[3] = "../Disease/Figowka_Gronkowcowa.html";
-links[4] = "../Disease/Osteoporoza.html";
+var links = new Array()
+links[ 0 ] = "../Disease/Borelioza.html"
+links[ 1 ] = "../Disease/Cukrzycowa_Choroba_Nerek.html"
+links[ 2 ] = "../Disease/Dna_Moczanowa.html"
+links[ 3 ] = "../Disease/Figowka_Gronkowcowa.html"
+links[ 4 ] = "../Disease/Osteoporoza"
 
 /*------------- Nie dotykać kodu pod tą linią -------------*/
 
 function RandomizePage() {
-    var a = Math.floor(Math.random() * links.length);
-    var i = a;
-    parent.location = links[i];
+	var a = Math.floor( Math.random() * links.length );
+	var i = a;
+	var last = links[ links.length - 1 ];
+	parent.location = links [ i ];
+	
+	/*
+	links.push( "Error.html" );
+	if ( i > last ) {
+		parent.location = links[ last ];
+	} else {
+		parent.location = links[ i ];
+	}
+	return false;
+	parent.location = links[ i ];
+	*/
+
 }
+
+/* 
+	Jak dodać kolejne choroby:
+		1. Skopiuj dokładną nazwę pliku choroby, np. "Osteoporoza.html".
+		2. Dodaj linię kodu "links[ x ] = "Plik.html", gdzie 'x' jest liczbą następującą po poprzedniej chorobie.
+		3. ???
+		4. Profit.
+*/
