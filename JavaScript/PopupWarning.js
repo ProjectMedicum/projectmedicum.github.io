d99074e443e78ca5248cee2a202af0b91bf5fdb6
Fayacre <johnbaton12312@hotmail.com>
2016-12-16 13:06:05 +0100

    Dużo fajności
    Dodano dwa nowe skrypty
    Dodano skrypt generujący do stron chorób
    Dodano skrypt powracający do stron choró
    Dodano skrypt wyświelający ostrzeżenie na stronie głównej
    Aktualizacja zawartości strony - Bliznowacenie Kłębuszków Nerek
    Aktualizacja zawartości strony - Template.html

---
+++ JavaScript/PopupWarning.js
@@ -0,0 +1,5 @@
+if (navigator.language == "pl") {
+    alert("Strona ma charakter edukacyjny i nie powinna być traktowana jako ekwiwalent konsultacji medycznej. Korzystasz z informacji dostępnych na stronie projectmedicum.github.io na własną odpowiedzialność.");
+} else {
+    alert("This website should serve only for educational purposes and it should not be used as the equivalent of a medical consultation. By entering this website you agree, that you take responsibility for how you use information avaible on website projectmedicum.github.io");
+}
--- JavaScript/RandomPage.js
+++ JavaScript/RandomPage.js
@@ -1,18 +1,24 @@
 //NIE CZYŚCIĆ KODU!
 
-var links = new Array()
-links[0] = "../Disease/Borelioza.html"
-links[1] = "../Disease/Cukrzycowa_Choroba_Nerek.html"
-links[2] = "../Disease/Dna_Moczanowa.html"
-links[3] = "../Disease/Figowka_Gronkowcowa.html"
-links[4] = "../Disease/Osteoporoza"
+var links = new Array();
+links[0] = "../Disease/Borelioza.html";
+links[1] = "../Disease/Cukrzycowa_Choroba_Nerek.html";
+links[2] = "../Disease/Dna_Moczanowa.html";
+links[3] = "../Disease/Figowka_Gronkowcowa.html";
+links[4] = "../Disease/Osteoporoza.html";
 
 /*------------- Nie dotykać kodu pod tą linią -------------*/
 
+
+function pushCurrentUrlToArray() { //prob not working
+    links.push(window.location.href);
+}
+
 function RandomizePage() {
     var a = Math.floor(Math.random() * links.length);
-    var i = a;
-    var last = links[links.length - 1];
-    parent.location = links[i];
-
+    if (a == links[links.length - 1]) {
+        return RandomizePage;
+    } else {
+        parent.location = links[a];
+    }
 }
