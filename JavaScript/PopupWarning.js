f76c6973ea1dcf2aa22dfd43cc1f1d4fba2ca12f
Fayacre <johnbaton12312@hotmail.com>
2016-12-17 18:17:06 +0100

    Aktualizacja tłumaczenia

--- JavaScript/PopupWarning.js
+++ JavaScript/PopupWarning.js
@@ -1,5 +1,7 @@
-if (navigator.language == "pl") {
-    alert("Strona ma charakter edukacyjny i nie powinna być traktowana jako ekwiwalent konsultacji medycznej. Korzystasz z informacji dostępnych na stronie projectmedicum.github.io na własną odpowiedzialność.");
-} else {
-    alert("This website should serve only for educational purposes and it should not be used as the equivalent of a medical consultation. By entering this website you agree, that you take responsibility for how you use information avaible on website projectmedicum.github.io");
-}
+$(document).ready(function() {
+    if (navigator.language == "pl") {
+        alert("Strona ma charakter edukacyjny i nie powinna być traktowana jako ekwiwalent konsultacji medycznej. Korzystasz z informacji dostępnych na stronie projectmedicum.github.io na własną odpowiedzialność.");
+    } else {
+        alert("This website should serve only for educational purposes and it should not be used as the equivalent of a medical consultation. By entering this website you agree, that you take responsibility for how you use information avaible on website projectmedicum.github.io");
+    }
+});
--- JavaScript/RandomPage.js
+++ JavaScript/RandomPage.js
@@ -1,7 +1,7 @@
 //NIE CZYŚCIĆ KODU!
 
 var links = new Array();
-links[0] = "../Disease/Borelioza.html";
+links[0] = "../Disease/Borelioza.html ";
 links[1] = "../Disease/Cukrzycowa_Choroba_Nerek.html";
 links[2] = "../Disease/Dna_Moczanowa.html";
 links[3] = "../Disease/Figowka_Gronkowcowa.html";
@@ -9,16 +9,18 @@ links[4] = "../Disease/Osteoporoza.html";
 
 /*------------- Nie dotykać kodu pod tą linią -------------*/
 
+/*function RandomizePage() {
+    var a = Math.floor(Math.random() * links.length);
+	parent.location = links[a];
+    }
+}*/
 
-function pushCurrentUrlToArray() { //prob not working
-    links.push(window.location.href);
-}
 
 function RandomizePage() {
-    var a = Math.floor(Math.random() * links.length);
-    if (a == links[links.length - 1]) {
-        return RandomizePage;
-    } else {
-        parent.location = links[a];
+    do {
+        var a = Math.floor(Math.random() * links.length);
     }
+    while (links[a] === window.location.href);
+
+    parent.location = links[a];
 }
