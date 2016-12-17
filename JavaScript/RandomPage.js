77636c4485ac787fb8fc594a67ca005ccdd9d5a3
Fayacre <johnbaton12312@hotmail.com>
2016-12-17 18:18:25 +0100

    test

--- JavaScript/RandomPage.js
+++ JavaScript/RandomPage.js
@@ -1,11 +1,11 @@
 //NIE CZYŚCIĆ KODU!
 
 var links = new Array();
-links[0] = "../Disease/Borelioza.html ";
-links[1] = "../Disease/Cukrzycowa_Choroba_Nerek.html";
-links[2] = "../Disease/Dna_Moczanowa.html";
-links[3] = "../Disease/Figowka_Gronkowcowa.html";
-links[4] = "../Disease/Osteoporoza.html";
+links[0] = "projectmedicum.github.io/Disease/Borelioza.html ";
+links[1] = "projectmedicum.github.io/Disease/Cukrzycowa_Choroba_Nerek.html";
+links[2] = "projectmedicum.github.io/Disease/Dna_Moczanowa.html";
+links[3] = "projectmedicum.github.io/Disease/Figowka_Gronkowcowa.html";
+links[4] = "projectmedicum.github.io/Disease/Osteoporoza.html";
 
 /*------------- Nie dotykać kodu pod tą linią -------------*/
 
